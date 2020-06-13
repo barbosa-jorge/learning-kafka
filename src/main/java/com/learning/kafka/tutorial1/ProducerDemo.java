261f99c7ad87b5480fe908de38a221d84020f3cd
Jorge Barbosa <jorgeluis6@gmail.com>
2020-06-13 14:36:58 -0700

    created first kafka producer

--- src/main/java/com/learning/kafka/tutorial1/ProducerDemo.java
+++ src/main/java/com/learning/kafka/tutorial1/ProducerDemo.java
@@ -1,9 +1,33 @@
 package com.learning.kafka.tutorial1;
 
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+import java.util.Properties;
+
 public class ProducerDemo {
 
     public static void main(String[] args) {
-        System.out.println("Hello");
+
+        String bootStrapServer = "localhost:9092";
+
+        Properties properties = new Properties();
+        properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootStrapServer);
+        properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+
+        // Creating producer
+        KafkaProducer<String, String> producer = new KafkaProducer<>(properties);
+
+        // Creating producer rercord
+        ProducerRecord<String, String> record = new ProducerRecord<>("first_topic", "Hello kafka");
+
+        // send data - asynchronous
+        producer.send(record);
+        // flush and close producer
+        producer.close();
 
     }
 }
